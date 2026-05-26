17dbf135c49f30b24c759f6cc63c5902dc08bb65
nos-es <ethemsahbaz@outlook.com>
2026-05-26 11:18:19 +0200

    testing gemini lib

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-def main():
-    print("Hello from ai-agent!")
+import os
+from dotenv import load_dotenv
+from google import genai
 
+load_dotenv()
+api_key = os.environ.get("GEMINI_API_KEY")
+client = genai.Client(api_key=api_key)
+response = client.models.generate_content(
+    model="gemini-2.5-flash", contents="Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum.")
 
-if __name__ == "__main__":
-    main()
+print(response.text)
