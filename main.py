b53825d024ff0623fb70c963d58e5eb4cc41deb4
nos-es <ethemsahbaz@outlook.com>
2026-05-27 13:55:34 +0200

    use prompt from cli args

--- main.py
+++ main.py
@@ -1,12 +1,17 @@
 import os
+import argparse
 from dotenv import load_dotenv
 from google import genai
 
 load_dotenv()
+parser = argparse.ArgumentParser(description="Chatbot")
+parser.add_argument("user_prompt", type=str, help="User Prompt")
+args = parser.parse_args()
 api_key = os.environ.get("GEMINI_API_KEY")
 client = genai.Client(api_key=api_key)
 response = client.models.generate_content(
-    model="gemini-2.5-flash", contents="Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum.")
+    model="gemini-2.5-flash", contents=args.user_prompt)
+
 
 if response.usage_metadata is not None:
     print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
