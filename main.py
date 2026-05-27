2896949ea4ec68f634cb2c885cb94f1bc4071a09
nos-es <ethemsahbaz@outlook.com>
2026-05-27 14:07:47 +0200

    handle conversion with multiple messages

--- main.py
+++ main.py
@@ -2,15 +2,19 @@ import os
 import argparse
 from dotenv import load_dotenv
 from google import genai
+from google.genai import types
 
 load_dotenv()
 parser = argparse.ArgumentParser(description="Chatbot")
 parser.add_argument("user_prompt", type=str, help="User Prompt")
 args = parser.parse_args()
+messages: list[types.Content] = [
+    types.Content(role="user", parts=[types.Part(text=args.user_prompt)])]
+
 api_key = os.environ.get("GEMINI_API_KEY")
 client = genai.Client(api_key=api_key)
 response = client.models.generate_content(
-    model="gemini-2.5-flash", contents=args.user_prompt)
+    model="gemini-2.5-flash", contents=messages)
 
 
 if response.usage_metadata is not None:
