4f967f4efc9087f499b80e94ec4b9b996828a764
nos-es <ethemsahbaz@outlook.com>
2026-05-27 18:33:22 +0200

    added calculator

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ from google.genai import types
 load_dotenv()
 parser = argparse.ArgumentParser(description="Chatbot")
 parser.add_argument("user_prompt", type=str, help="User Prompt")
+parser.add_argument("--verbose", action="store_true", help="Enable verbose output")
 args = parser.parse_args()
 messages: list[types.Content] = [
     types.Content(role="user", parts=[types.Part(text=args.user_prompt)])]
@@ -17,7 +18,8 @@ response = client.models.generate_content(
     model="gemini-2.5-flash", contents=messages)
 
 
-if response.usage_metadata is not None:
+if response.usage_metadata is not None and args.verbose:
+    print(f"User prompt: {args.user_prompt}")
     print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
     print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
 
