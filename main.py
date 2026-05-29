0eecf0c7abd78795d96611d7a5e6169580e96d24
nos-es <ethemsahbaz@outlook.com>
2026-05-29 10:30:16 +0200

    added system prompt to client config

--- main.py
+++ main.py
@@ -3,19 +3,24 @@ import argparse
 from dotenv import load_dotenv
 from google import genai
 from google.genai import types
+from prompts import system_prompt
 
 load_dotenv()
 parser = argparse.ArgumentParser(description="Chatbot")
 parser.add_argument("user_prompt", type=str, help="User Prompt")
-parser.add_argument("--verbose", action="store_true", help="Enable verbose output")
+parser.add_argument("--verbose", action="store_true",
+                    help="Enable verbose output")
 args = parser.parse_args()
 messages: list[types.Content] = [
     types.Content(role="user", parts=[types.Part(text=args.user_prompt)])]
 
 api_key = os.environ.get("GEMINI_API_KEY")
 client = genai.Client(api_key=api_key)
+
 response = client.models.generate_content(
-    model="gemini-2.5-flash", contents=messages)
+    model="gemini-2.5-flash",
+    config=types.GenerateContentConfig(system_instruction=system_prompt),
+    contents=messages)
 
 
 if response.usage_metadata is not None and args.verbose:
