f858bd2ddf93fa9984fd619a4e748c8cf9bc4c1d
nos-es <ethemsahbaz@outlook.com>
2026-05-30 10:29:31 +0200

    agent calling functions

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from dotenv import load_dotenv
 from google import genai
 from google.genai import types
 from prompts import system_prompt
-from call_function import available_functions
+from call_function import available_functions, call_function
 
 load_dotenv()
 parser = argparse.ArgumentParser(description="Chatbot")
@@ -33,7 +33,17 @@ if response.usage_metadata is not None and args.verbose:
 
 if response.function_calls:
     for function_call in response.function_calls:
-        print(f"Calling function: {function_call.name}({function_call.args})")
+        function_call_result = call_function(function_call, args.verbose)
+        if function_call_result.parts[0].function_response is None:
+            raise Exception(
+                f"Error: FunctionResponseObject was None for the function{function_call}")
+
+        if function_call_result.parts[0].function_response.response is None:
+            raise Exception(
+                f"Error: response Property of FunctionResponseObject was None for the function{function_call}")
+        if args.verbose:
+            print(
+                f"-> {function_call_result.parts[0].function_response.response}")
 
 
 print(f"Response: {response.text}")
